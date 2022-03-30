1c72f0cdbb657e966a16bacb851d67950e87c01c
Jan Winkelmann (keks) <git@keks.cool>
2022-03-30 23:05:55 +0200

    add examples of real syntax and new grammar

--- src/bin/parser.rs
+++ src/bin/parser.rs
@@ -7,12 +7,12 @@ use std::env;
 use std::fs;
 
 #[derive(Parser)]
-#[grammar = "parser/ssp.pest"]
+#[grammar = "parser/pkg.pest"]
 pub struct SspParser;
 
-fn main () {
+fn main() {
     let args: Vec<String> = env::args().collect();
     let filecontent = fs::read_to_string(args[1].clone()).expect("cannot read file");
-    let result = SspParser::parse(Rule::code, &filecontent);
+    let result = SspParser::parse(Rule::package, &filecontent);
     println!("{:#?}", result);
 }
