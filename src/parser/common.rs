8dac67738b3e0763b6fab93646dfbb5c512b863c
Jan Winkelmann (keks) <git@keks.cool>
2022-04-13 22:38:35 +0200

    parser: support type_tuple in handle_type

--- src/parser/common.rs
+++ src/parser/common.rs
@@ -9,12 +9,13 @@ pub fn handle_type(tipe: Pair<Rule>) -> Type {
         Rule::type_integer => Type::Integer,
         Rule::type_maybe => Type::Maybe(Box::new(handle_type(tipe.into_inner().next().unwrap()))),
         Rule::type_bits => Type::Bits(tipe.into_inner().next().unwrap().as_str().to_string()),
+        Rule::type_tuple => Type::Tuple(tipe.into_inner().map(handle_type).collect()),
         Rule::type_table => {
             let mut inner = tipe.into_inner();
             let indextype = handle_type(inner.next().unwrap());
             let valuetype = handle_type(inner.next().unwrap());
             Type::Table(Box::new(indextype), Box::new(valuetype))
-        },
+        }
         Rule::type_fn => {
             let mut inner = tipe.into_inner();
             let argtipes = inner
