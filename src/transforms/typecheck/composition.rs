dfaca9235649730942b3586d0db07b26512dc75d
Christoph Egger <christoph@christoph-egger.org>
2022-05-04 21:42:18 +0200

    fix error messaging

--- src/transforms/typecheck/composition.rs
+++ src/transforms/typecheck/composition.rs
@@ -50,7 +50,13 @@ pub fn typecheck_comp(
         edge_imports.entry(*from).or_insert(HashSet::new()).insert(sig_.clone());
     }
     if declared_imports != edge_imports {
+        if declared_imports.keys().collect::<Vec<_>>() != edge_imports.keys().collect::<Vec<_>>() {
+            panic!("Different set of keys with imports, declared: {:?} edges: {:?}", declared_imports.keys(), edge_imports.keys())
+        }
         for (i, pkg) in pkgs.clone().iter().enumerate() {
+            if !declared_imports.contains_key(&i) {
+                continue;
+            }
             if declared_imports[&i] != edge_imports[&i] {
                 panic!("package: {} declared: {:?} edges: {:?}", pkg.name, declared_imports[&i], edge_imports[&i]);
             }
