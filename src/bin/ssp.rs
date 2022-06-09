a135d78a1439601bb2947415f65d6e3af6fca0d0
Christoph Egger <christoph@christoph-egger.org>
2022-06-09 18:01:19 +0200

    `ssp smt` command

--- src/bin/ssp.rs
+++ src/bin/ssp.rs
@@ -3,6 +3,9 @@ use clap::{Parser, Subcommand};
 use sspds::cli::filesystem::{parse_composition, parse_packages, read_directory};
 use sspds::tex::writer::{tex_write_composition};
 use sspds::package::{Composition, Edge, Export};
+use sspds::hacks;
+use sspds::smt::exprs::SmtFmt;
+use sspds::smt::writer::CompositionSmtWriter;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
@@ -19,6 +22,8 @@ struct Cli {
 enum Commands {
     /// Verifies the code of packages
     Check { name: String },
+    /// Generates SMT
+    Smt { name: String },
     /// Generate latex (cryptocode) files
     Latex(LaTeX),
     /// Generate graph representation of the composition
@@ -86,6 +91,36 @@ fn graph(args: &Graph) {
     }
 }
 
+fn smt(name:&String) {
+    let (pkgs_list, comp_list) = read_directory(&name);
+    let (pkgs_map, _pkgs_filenames) = parse_packages(&pkgs_list);
+    let comp_map = parse_composition(&comp_list, &pkgs_map);
+
+    hacks::declare_par_Maybe();
+    hacks::declare_Tuple(2);
+    println!("(declare-sort Bits_n 0)");
+    println!("(declare-fun f (Bits_n Bits_n) Bits_n)");
+
+    for (name, comp) in comp_map {
+        println!("; {}", name);
+
+        //println!("{:#?}", comp);
+        let (comp, _) = match sspds::transforms::transform_all(&comp) {
+            Ok(x) => x,
+            Err(e) => {
+                panic!("found an error in composition {}: {:?}", name, e)
+            }
+        };
+        let writer = CompositionSmtWriter::new(&comp);
+        for line in writer.smt_composition_all() {
+            line.write_smt_to(&mut std::io::stdout()).unwrap();
+        }
+    }
+
+    println!("(check-sat)");
+}
+
+
 fn check(args: &String) {
     let (pkgs_list, comp_list) = read_directory(&args);
     let (pkgs_map, _pkgs_filenames) = parse_packages(&pkgs_list);
@@ -119,6 +154,7 @@ fn main() {
 
     match &cli.command {
         Commands::Check { name } => check(name),
+        Commands::Smt { name } => smt(name),
         Commands::Latex(args) => latex(args),
         Commands::Graph(args) => graph(args),
     }
