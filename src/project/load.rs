edb98dd4754699065f41fc8c4ea925d5b5f78e45
Jan Winkelmann (keks) <git@keks.cool>
2022-09-21 21:21:15 +0200

    don't expect a reduction direction; update example projects

--- src/project/load.rs
+++ src/project/load.rs
@@ -89,7 +89,7 @@ pub enum TomlGameHop {
         left: String,
         right: String,
         assumption: String,
-        direction: String,
+        //direction: String,
         // we probably have to provide more information here,
         // in order to easily figure out how to perform the rewrite
     },
