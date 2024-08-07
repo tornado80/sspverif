443fc8e1d6251ed5d1e158e188d60b7163bdc1fc
Jan Winkelmann (keks) <jan@cryspen.com>
2024-08-07 22:21:47 +0200

    add test

--- src/parser/tests/sound/games.rs
+++ src/parser/tests/sound/games.rs
@@ -53,3 +53,16 @@ fn missing_game_empty_block() {
     let report = miette::Report::new(err);
     println!("{report:?}");
 }
+
+#[test]
+fn param_wrong_type() {
+    let pkgs = packages::parse_files(&["PRF.pkg.ssp", "KeyReal.pkg.ssp", "Enc.pkg.ssp"]);
+    let err = games::parse_file_fails("Game-param-wrong-type-should-fail.comp.ssp", &pkgs);
+
+    let ParseGameError::ParseExpression(ParseExpressionError::TypeMismatch(err)) = &err else {
+        panic!("expected different error, got {}", err);
+    };
+
+    assert_eq!(err.expected, Type::Integer);
+    assert_eq!(err.got, Type::Bits("n".to_string()));
+}
