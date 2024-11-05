490c00b401d4c9d2de12ca1a37434f537a8aa286
Jan Winkelmann (keks) <jan@cryspen.com>
2024-11-05 23:22:05 +0100

    better naming for relations

--- src/writers/smt/patterns/functions/relation.rs
+++ src/writers/smt/patterns/functions/relation.rs
@@ -10,7 +10,10 @@ use super::FunctionPattern;
 
 #[derive(Clone, Copy, Debug)]
 pub(crate) struct Relation<'a> {
-    pub(crate) name: &'a str,
+    pub(crate) game_inst_name_left: &'a str,
+    pub(crate) game_inst_name_right: &'a str,
+    pub(crate) relation_name: &'a str,
+    pub(crate) oracle_name: &'a str,
     pub(crate) state_datatype_left: GameStatePattern<'a>,
     pub(crate) state_datatype_right: GameStatePattern<'a>,
     pub(crate) return_datatype_left: ReturnPattern<'a>,
@@ -36,7 +39,13 @@ impl<'a> Relation<'a> {
 
 impl<'a> FunctionPattern for Relation<'a> {
     fn function_name(&self) -> String {
-        self.name.to_string()
+        format!(
+            "<relation-{}-{}-{}-{}>",
+            self.relation_name,
+            self.game_inst_name_left,
+            self.game_inst_name_right,
+            self.oracle_name
+        )
     }
 
     fn function_args(&self) -> Vec<(String, Sort)> {
