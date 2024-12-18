3d8fa68644b892d6392c55e3ba6a060f3c6b6183
Christoph Egger <christoph@christoph-egger.org>
2024-12-18 22:12:23 +0100

    ambiguity

--- src/writers/tex/writer.rs
+++ src/writers/tex/writer.rs
@@ -77,16 +77,16 @@ impl<'a> BlockWriter<'a> {
                 .map(|expr| self.expression_to_tex(expr))
                 .collect::<Vec<_>>()
                 .join(" = "),
-            Expression::Or(exprs) => exprs
+            Expression::Or(exprs) => format!("({})", exprs
                 .iter()
                 .map(|expr| self.expression_to_tex(expr))
                 .collect::<Vec<_>>()
-                .join(" \\vee "),
-            Expression::And(exprs) => exprs
+                .join(" \\vee ")),
+            Expression::And(exprs) => format!("({})", exprs
                 .iter()
                 .map(|expr| self.expression_to_tex(expr))
                 .collect::<Vec<_>>()
-                .join(" \\wedge "),
+                .join(" \\wedge ")),
             Expression::Tuple(exprs) => {
                 format!(
                     "({})",
